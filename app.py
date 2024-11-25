d53b51739a08c37319276cf017d59cf335bbdac5
JasonD23 <jasondsouza@radixplore.com>
2024-11-25 19:02:07 +0530

    removed unncessary components

--- app.py
+++ app.py
@@ -141,17 +141,9 @@ st.subheader('Made by Jason Dsouza')
 uploaded_file = st.file_uploader("Choose an image", type=['png', 'jpg', 'jpeg'])
 if uploaded_file:
     filename = uploaded_file.name
-    progress_text = "Processing image..."
-    progress_bar = st.progress(0)
-    status_text = st.empty()    
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    progress_bar.progress(25)
     image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    progress_bar.progress(50)
-
     tiff_data = process_single_image(image, filename)
-    progress_bar.progress(100)
-
     if tiff_data:
         st.success("GeoTIFF successfully created!")
         st.download_button(
@@ -160,6 +152,3 @@ if uploaded_file:
             file_name=f'{os.path.splitext(filename)[0]}.tif',
             mime='image/tiff'
         )
-    else:
-        progress_bar.empty()
-        status_text.error("Failed to process image")
